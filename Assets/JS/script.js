4b758bb417f45f967b36b3bf011a964e8f95eee2
LBDCode <libby.duggan@gmail.com>
2018-11-05 09:17:46 -0500

    final files

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
-var level = 1
-var curBank = 'wordBank' + level;
+
 var ans = "";
 var ansArr = [];
 var guessArr = [];
@@ -11,12 +10,11 @@ var wrongLetters = [];
 var guess;
 var possGuesses = "qwertyuiopasdfghjklzxcvbnm";
 
+
 var game = {    
-    wordBank1: ["time","person","year","way","day","thing","man","world","life","hand",
-        "part","child","eye","woman","place","work","week","case","point","government","company",
-        "number","group","problem","fact","good","new","first","last","long","great","little","own",
-        "other","old","right","big","high","different","small","large","next","early","young",
-        "important","few","public","bad","same","able"],
+    wordBank1: ["acorn","apple","autumn","chestnuts","cider","cobweb","cold","corn","cornucopia",
+        "fall","feast","harvest","haystack","jackey","leaves","pumpkin","pie","scarecrow","squash",
+        "stuffing","thanksgiving","turkey"],
 
     randNum: function (min, max) {
         min = Math.ceil(min);
@@ -34,7 +32,7 @@ var game = {
     updateGuessArr: function () {
         for (var i = 0; i < ansArr.length; i++){
             guessArr[i] = "_"
-        };
+        }
     },
 
     checkGuess: function() {
@@ -43,46 +41,43 @@ var game = {
                 if (ansArr[i] === guess) {
                     guessArr[i] = guess;
                 };
-            };
+            }
+            if (guessArr.join("") === ans) {
+                game.win();
+            } 
         } else {
             if (wrongLetters.includes(guess)) {
-                console.log("You already tried this guess");
+                alert("You already tried that letter.");
             } else {
                 wrongLetters.push(guess);
                 --guessesRemain;
-            };
-        };
+            }
+        }
     },
 
     updateUI: function() {
-        document.getElementById("ans").textContent = "Answer: " + ans;
-        document.getElementById("guess").textContent = "Current Guess: " + guessArr.join(" ");
+        document.getElementById("guess").textContent = guessArr.join(" ");
         document.getElementById("score").textContent = "Score: " + score;
         document.getElementById("guessesRemain").textContent = "Remaining Guesses: " + guessesRemain;
         document.getElementById("wrongLetters").textContent = "Incorrect Guesses: " + wrongLetters.join(",");
+        document.getElementById("message").textContent = "";
     },
     
-    newGame: function() {
-        game.init();
-        game.chooseWord();
-        game.updateGuessArr();
-        game.updateUI();
-    },
-
+   
     win: function() {
-        ++score;
-        game.chooseWord();
-        game.updateGuessArr();
-        game.updateUI();
+        score++;
+        guessesRemain = 10;
+        this.chooseWord();
+        this.updateGuessArr();
+        this.updateUI();
+        
     },
 
-    endGame: function () {
-        console.log("Better luck next time!");
+    endGame: function() {
+        alert("Sorry, the answer was: " + ans + ". Better luck next time!");
     },
 
     init: function() {
-        level = 1
-        curBank = 'wordBank' + level;
         ans = "";
         ansArr = [];
         guessArr = [];
@@ -92,16 +87,15 @@ var game = {
         guess;
     },
 
-    continuePlay: function() {
-        if (guessesRemain > 0) {
-        game.checkGuess();
+    newGame: function() {
+        game.init();
+        game.chooseWord();
+        game.updateGuessArr();
         game.updateUI();
-        } else {
-            game.endGame();
-        }
     },
+
          
-  }
+}
  
   
 document.getElementById("newGame").addEventListener("click", game.newGame); 
@@ -109,10 +103,11 @@ document.getElementById("newGame").addEventListener("click", game.newGame);
 document.onkeypress = function(event) {
     guess = event.key.toLowerCase();
     if (possGuesses.includes(guess)) {
-        if (guessArr.join("") === ans) {
-        game.win();
+        if (guessesRemain > 0) {
+            game.checkGuess();
+            game.updateUI();
         } else {
-        game.continuePlay(); 
+            game.endGame();
         };
     };
 };
@@ -120,13 +115,5 @@ document.onkeypress = function(event) {
 
 
 
-// var wordBank2 = ["Awkward","Bagpipes","Banjo","Bungler","Croquet","Crypt","Dwarves",
-// "Fervid","Fishhook","Fjord","Gazebo","Gypsy","Haiku","Haphazard","Hyphen","Ivory",
-// "Jazzy","Jiffy","Jinx","Jukebox","Kayak","Kiosk","Klutz","Memento","Mystify",
-// "Numbskull","Ostracize","Oxygen","Pajama","Phlegm","Pixel","Polka","Quad","Quip",
-// "Rhythmic","Rogue","Sphinx","Squawk","Swivel","Toady","Twelfth","Unzip","Waxy","Wildebeest",
-// "Yacht","Zealous","Zigzag","Zippy","Zombie"];
 
 
-
-//still left to do: levels
